7e8e5efc29c46316aefe1c1e2e07476586712833
Daz Jones <yuki@thebronasium.com>
2013-11-24 17:21:07 +0000

    Merge Y300-J1 release (19/11/2013)
    http://consumer.huawei.com/en/support/downloads/detail/index.htm?id=17937
    
    Conflicts:
    	drivers/media/video/msm/torch.c

--- drivers/media/video/msm/torch.c
+++ drivers/media/video/msm/torch.c
@@ -109,18 +109,20 @@ static long hw_camera_led_ioctl(struct file *filep ,unsigned int cmd, unsigned l
 				/*these handset use tps61310 as flash*/
 				if( machine_is_msm8x25_U8825()
 				|| machine_is_msm8x25_U8825D()
-                || machine_is_msm8x25_U8833D()
-                || machine_is_msm8x25_U8833()                
+                                || machine_is_msm8x25_U8833D()
+                                || machine_is_msm8x25_U8833()                
 				|| machine_is_msm8x25_C8825D()
 				|| machine_is_msm8x25_C8950D()
 				|| machine_is_msm8x25_U8950D()
 				|| machine_is_msm8x25_U8951()
-                || machine_is_msm8x25_C8813()
-                || machine_is_msm8x25_H881C()
-				||machine_is_msm8x25_U8950())
+				|| machine_is_msm8x25_C8813()
+				|| machine_is_msm8x25_H881C()
+				|| machine_is_msm8x25_U8950()
+				|| machine_is_msm8x25_Y300_J1()
+				|| machine_is_msm7x27a_U8815())
 				{
 			
-					tps61310_set_flash(camera_led_state);
+					ret = call_led_set_state(camera_led_state);
 				}
 				else
 				{
